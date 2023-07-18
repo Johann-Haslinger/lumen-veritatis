68a84851ea856d1b025543fd53d760796062aef0
ValoriaStudios <heyjohn655@gmail.com>
2023-07-18 14:24:05 +0200

    screen hanlder

--- src/pages/ScreenHandler.tsx
+++ src/pages/ScreenHandler.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import {
-  useEntities,
-  useEntity,
-  useEntityComponents,
-} from "@leanscope/ecs-engine";
-import { GameStateFacet } from "../app/GameFacets";
-import Game from "./Game";
+import React, { useEffect } from 'react';
+import { Entity, useEntities, useEntity, useEntityComponents } from '@leanscope/ecs-engine';
+import { GameStateFacet } from '../app/GameFacets';
+import Game from './Game';
+import { EntityProps } from '@leanscope/ecs-engine/react-api/classes/EntityProps';
 
+const Handler = (props: EntityProps) => {
+  const gameStateFacet = useEntityComponents(props.entity, GameStateFacet);
+  console.log(gameStateFacet);
+  return <div className=" bg-black   fixed top-0">{true && <Game />}</div>;
+};
 
 const ScreenHandler = () => {
-  // const [gameStateEntity] = useEntity((e) => e.has(GameStateFacet));
-  const [gameStateEntities] = useEntities((e) =>
-    e.hasComponent(GameStateFacet)
-  );
-  const gameStateEntity = gameStateEntities[0];
-  // const [components] = useEntityComponents(gameStateEntity);
-  console.log(gameStateEntity);
-
-  return <div className=" bg-black   fixed top-0">{true && <Game />}</div>;
+  const [gameStateEntities] = useEntities((e) => e.hasComponent(GameStateFacet));
+  
+  useEffect(() => {
+    console.log(gameStateEntities.length);
+  }, [gameStateEntities]);
+  // console.log(gameStateEntities.length)
+  return <>{gameStateEntities.length !== 0 && <Handler entity={gameStateEntities[0]} />}</>;
 };
 
 export default ScreenHandler;
